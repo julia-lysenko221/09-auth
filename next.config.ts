00e6850cad5420c9dadb3efea858722581d39ab4
juliya.lysenko <juliya.deleh@gmail.com>
2025-08-29 13:59:19 +0300

    commit4

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-// \
-// next.config.ts
-
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'picsum.photos' }],
+    remotePatterns: [{ protocol: 'https', hostname: 'ac.goit.global' }],
   },
 };
 
